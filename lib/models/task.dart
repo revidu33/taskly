36b3bd60d6ecce58b342ecbd4d19b4f9c238a7be
Revidu <revidu33@gmail.com>
2024-07-12 04:55:59 +0100

    add conversion to json

--- lib/models/task.dart
+++ lib/models/task.dart
@@ -1,11 +1,31 @@
-// models/task.dart
-
 class Task {
-  final String title;
-  final String description;
+  int? id;
+  String title;
+  String description;
+  String dueDate;
+  bool? _isComplete;
 
-  Task({
-    required this.title,
-    required this.description,
-  });
+  Task(
+      {this.id,
+      required this.title,
+      required this.description,
+      required this.dueDate});
+
+  Map<String, dynamic> toMap() {
+    var map = <String, dynamic>{
+      'title': title,
+      'description': description,
+      'dueDate': dueDate,
+    };
+    if (id != null) {
+      map['id'] = id;
+    }
+    return map;
+  }
+
+  Task.fromMap(Map<String, dynamic> map)
+      : id = map['id'],
+        title = map['title'],
+        description = map['description'],
+        dueDate = map['dueDate'];
 }
